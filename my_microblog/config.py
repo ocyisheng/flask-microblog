1be5b69ca72e491576e7b7a5e885b07503e59c52
gao <gao@huanglongdewifi.local>
2019-06-13 18:20:44 +0800

    form

--- my_microblog/config.py
+++ my_microblog/config.py
@@ -8,3 +8,12 @@
 # 激活跨站点请求伪造保护
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess!'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accoutns/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'},
+
+]
